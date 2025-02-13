0ada75e816e4d167f55db01e6876dcab16e24a45
KennetAvila28 <19091097@alumno.utmetropolitana.edu.mx>
2025-02-13 11:47:20 -0600

    add dest and remitent in the message

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { join } from "node:path"
 import {Server} from "socket.io"
 import cors from "cors"
 import { v4 } from "uuid"
+import { log } from "node:console"
 const app = express()
 const server = createServer(app)
 const io = new Server(server, {cors:{
@@ -28,9 +29,9 @@ server.listen(3000, ()=> {
 
 io.on("connection", (socket)=>{
     console.log(socket.id)
-    socket.on(socket.id,(message, dest)=>{
-        
-        io.emit(dest, message)
+    socket.on("message",(message, dest, from)=>{
+        log({message, dest, from})
+        io.emit(dest, message, from)
     })
     const setup =  {
         id:socket.id,
